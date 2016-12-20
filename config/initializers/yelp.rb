81683562803117d422e490ccadba95c90608f8dd
Iggy <igoririanto@rocketmail.com>
2016-12-20 13:21:26 -0800

    hide the keys

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,6 +1,3 @@
 Yelp.client.configure do |config|
-  config.consumer_key = 'up5BdrrESxn0y4xszrVn8g'
-  config.consumer_secret = '5auz6zuyigKkHDXhnvWPlt8CqJk'
-  config.token = 't9rRR_OEx-2GAZIx1-biYOSd0L6GzT7m'
-  config.token_secret = 'klpHmos0cgITGG2mpEF_xBC_GQ0'
+
 end
