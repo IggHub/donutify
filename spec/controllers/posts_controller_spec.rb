1646f1a82816330989bd2c4ed2138caab89d7930
Iggy <igoririanto@rocketmail.com>
2017-02-03 11:22:10 -0800

    add heroku gems

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,13 +3,7 @@ require 'rails_helper'
 describe PostsController do
   describe "POST scrape" do
     it "creates new restaurant attributes from yelp"
-    it "saves new restaurant attributes from yelp`"
-
-    it "redirect to Posts index" do
-      Post.destroy_all
-      results = Yelp.client.search('moreno valley', {limit: 1, category_filter: "donuts"})
-      post :scrape
-      response.should redirect_to(action: 'index')
-    end
+    it "saves new restaurant attributes from yelp"
+    it "redirect to Posts index"
   end
 end
