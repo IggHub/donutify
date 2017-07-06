67f7997c751547c02d6e6457146d96dee5c8a016
Iggy <igoririanto@rocketmail.com>
2017-07-05 19:36:15 -0700

    add stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  consumer_key: 'up5BdrrESxn0y4xszrVn8g'
+  consumer_secret: '5auz6zuyigKkHDXhnvWPlt8CqJk'
+  token: 't9rRR_OEx-2GAZIx1-biYOSd0L6GzT7m'
+  token_secret: 'klpHmos0cgITGG2mpEF_xBC_GQ0'
 end
